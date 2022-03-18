a3781174802e31dbee9538427f2f4712086b1120
Stephen Gomez-Fox <stephen@gomezfox.net>
2022-03-18 13:10:15 -0700

    Adds WordDao data access object

---
+++ app/src/main/java/com/zybooks/roomwordsample/WordDao.java
@@ -0,0 +1,19 @@
+package com.zybooks.roomwordsample;
+
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
+public interface WordDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Word word);
+
+    @Query("DELETE FROM word_table")
+    void deleteAll();
+
+    @Query("SELECT * FROM word_table ORDER BY word ASC")
+    List<Word> getAlphabetizedWords();
+
+}
